bf9f4deb006e7143e8c5c092fda48be0eb44fd0d
ChiaraPontello <chiarapontello1@gmail.com>
2023-11-22 11:30:40 +0100

    HeaderComponents html-css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="App">
-
+        <HeaderComponent />
     </div>
 </template>
 
@@ -16,5 +16,7 @@ import HeaderComponent from './components/HeaderComponent.vue'
 </script>
 
 <style lang="scss" scoped>
+
+  @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@700&family=Lato:wght@100;300;400&family=Montserrat:wght@400;600&family=Noto+Sans:wght@300;600&family=PT+Sans:wght@400;700&family=Roboto:wght@300;500;900&display=swap');
 
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,61 @@
 <template>
-    <div>
-
-    </div>
+    <!--sezione destra-->
+    <header class="intro">
+        <div >
+            <img src="../assets/image/restaurant.jpg" class="restaurant">
+        </div>
+        
+        <!--sezione sinistra-->
+        <div class="leftSection " >
+            <div class="fusion">
+            <span class="text-uppercase">the best table in town </span>
+            <h1 class="text-uppercase">fine dining experience</h1>
+            <p>Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.</p>
+            
+            <button type="button" class="btn btn-outline-light buy">BUY AVADA TODAY</button>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
     export default {
+        name: 'HeaderComponent'
         
     }
 </script>
 
 <style lang="scss" scoped>
+.intro {
+    background-color: black;
+}
+.restaurant{
+    width: 50%;
+    height: 100vh;
+    float: right;
+}
+p{
+    color: white;
+    padding: 10px;
+    font-size: 1.3rem;
+}
+h1{
+    color: white;
+    font-size: 5rem;
+}
+span{
+    color: white;
+    font-size: 1.5rem;
+}
+.leftSection{
+width: 100%;
+height: 100vh;
+}
+.fusion{
+    padding-top: 15%;
+    padding-left: 10%;
+    
+}
 
 </style>
+
