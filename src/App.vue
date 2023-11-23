ecbf489d84b7b6ce8d015230b4cf02b2b7e2bf96
ChiaraPontello <chiarapontello1@gmail.com>
2023-11-23 15:19:22 +0100

    create FooterComponent

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div id="App">
         <HeaderComponent />
         <MainComponent />
+        <FooterComponent />
     </div>    
        
     
@@ -11,11 +12,13 @@
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
     export default {
         name: 'App',
         components: {
             HeaderComponent,
             MainComponent,
+            FooterComponent,
         }
         
     }
